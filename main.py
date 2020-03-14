e4575fdd49b4c9d0f6a5a3b62245c8812902e7de
MKofficial <matej.kriz2003@gmail.com>
2020-03-14 20:03:49 +0100

    improve user input

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 version_control = 'v0.1.0'
 version = f'K9 transform program.exe\nVersion: {version_control}\n'
-problem_message = ['We could not find your file, because the file do not exists or the path is wrong']
+problem_message = ['We could not find your file, because the file do not exists or the path is wrong',
+                   'I do not understand your command']
 
 if __name__ == '__main__':
     print(version)
@@ -22,7 +23,15 @@ if __name__ == '__main__':
     print('Save all of your\'s excel workbooks...we need to close them')
 
     # ask user to run the program
-    run = input('Run? (Y/N): ').lower()
-    if run in ['n', 'no', 'ne']:
-        quit()
-    os.system('taskkill /F /IM excel.exe')
+    while True:
+        run = input('Run? (Y/N): ').lower()
+        if run in ['n', 'no', 'ne']:
+            quit()
+        elif run in ['y', 'yes', 'ano', 'a']:
+            # kill excel app and all open workbooks
+            os.system('taskkill /IM excel.exe')
+            break
+        else:
+            print(problem_message[1])
+            input('Press any key to continue')
+            print()
