64065cd610b94049f666b4b236eedd12639136e6
MKofficial <matej.kriz2003@gmail.com>
2020-03-14 14:16:58 +0100

    path input and killing excel

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+version_control = 'v0.1.0'
+version = f'K9 transform program.exe\nVersion: {version_control}\n'
+problem_message = ['We could not find your file, because the file do not exists or the path is wrong']
+
+if __name__ == '__main__':
+    print(version)
+    g_race_path = input('Type an absolute path to your file: ')
+
+    # search for slashes or backslashes in the path
+    # if there is not any, raise error
+    if g_race_path.find('\\') != -1:
+        g_race_file = g_race_path.split('\\')[-1]
+    elif g_race_path.find('/') != -1:
+        g_race_file = g_race_path.split('/')[-1]
+    else:
+        print('error\n'.upper() + f'You try to open \"{g_race_path}\"\n\n' + f'Problem: {problem_message[0]}')
+        input('Press any key to quit')
+        quit()
+
+    print('Save all of your\'s excel workbooks...we need to close them')
+
+    # ask user to run the program
+    run = input('Run? (Y/N): ').lower()
+    if run in ['n', 'no', 'ne']:
+        quit()
+    os.system('taskkill /F /IM excel.exe')
