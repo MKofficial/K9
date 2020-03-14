6770300e8c4fc35edad2e499d530b93af69f7dc3
MKofficial <matej.kriz2003@gmail.com>
2020-03-15 00:22:15 +0100

    add uncompleted feature for cols values

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 import openpyxl as opx
 from openpyxl.utils.exceptions import InvalidFileException
+from my_utils import problem_message, set_cols_num
 
 
 def get_file_arr(excel_file: str = None) -> list:
@@ -16,15 +17,18 @@ def get_file_arr(excel_file: str = None) -> list:
                                                                              'Please rerun the program again with the '
                                                                              'right file format')
         input('\nPress any key to continue')
-        wb = opx.Workbook()
-
+        quit()
     except FileNotFoundError:
         print('Error!\n'.upper() + f'You try to open \"{excel_file}\"\n\n' + 'There is an error in finding your file\n'
                                                                              'Please rerun the program again with the '
-                                                                             'right file name')
+                                                                             'right file name or path')
         input('\nPress any key to continue')
         quit()
     else:
-        pass
-
-    wb.save(excel_file)
+        file = open('setup_cols.txt', 'r+', encoding='utf-8')
+        if file.read() is None:
+            for param in set_cols_num:
+                file.write(f'{param}: ' + input('Set column index ("A" -> 1) for ' + param.upper() + ': ') + '\n')
+        elif file.read() is not None:
+            pass
+        file.close()
