d25db11c05d5a8162dd5c9b98250b2480ebbeeeb
MKofficial <matej.kriz2003@gmail.com>
2020-03-25 22:14:09 +0100

    utils: make dist_and_cat, get_data: make add_points(), get_complete_file_arr(), get_final_arr(), improve get_file_arr()

--- get_data.py
+++ get_data.py
@@ -1,12 +1,14 @@
 import openpyxl as opx
 from openpyxl.utils.exceptions import InvalidFileException
-from my_utils import problem_message, set_cols_num, raise_error
+from my_utils import problem_message, set_cols_num, raise_error, dist_and_cat
+
+unfinished = ['DNF', 'DSQ', 'DNS']
 
 
 def setup_columns(file_txt: str = 'setup_cols.txt') -> list:
     """
     :param file_txt: File which will contain specific index for each column
-    :return: List with
+    :return: List with number of each column
     """
     with open(file_txt, 'r', encoding='utf-8') as file:
         if file.read() == '':
@@ -35,8 +37,37 @@ def setup_columns(file_txt: str = 'setup_cols.txt') -> list:
     return arr
 
 
+def add_points(people: list = [], multiply: bool = False) -> list:
+    """
+    :param people: List with people to be added points as their last element
+    :param multiply: This multiply the points. Usage only if the K9 is on the sixth race
+    :return: List with people containing points now
+    """
+    points = 11
+    if multiply:
+        points *= 1.5
+
+    for person in people:
+        if person[2] in unfinished:
+            for i in unfinished:
+                if person[2] == i:
+                    person.append(i)
+                    del person[2]
+        if points == 10:
+            points -= 1
+        if points <= 0:
+            person.append(0)
+            del person[2]
+        else:
+            person.append(points)
+            points -= 1
+            del person[2]
+
+    return people
+
+
 def get_file_arr(excel_file: str = 'kuneticka_devitka.xlsx', distance: str = 'Dlouhý okruh - 9 km',
-                 category: str = 'Muži 18 - 29 let') -> list:
+                 category: str = 'Muži 18 - 29 let', sixth_race: bool = False) -> list:
     """
     :param excel_file: Excel file with the newest race's results
     :param category: Person's category
@@ -53,7 +84,6 @@ def get_file_arr(excel_file: str = 'kuneticka_devitka.xlsx', distance: str = 'Dl
         raise_error(excel_file, problem_message[0])
     else:
         index = [i for i in setup_columns()]
-        print(index)
 
     people = [[ws[person.row][index[0]].value, ws[person.row][index[1]].value,
                ws[person.row][index[2]].value, ws[person.row][index[3]].value,
@@ -61,8 +91,36 @@ def get_file_arr(excel_file: str = 'kuneticka_devitka.xlsx', distance: str = 'Dl
               for i in ws.iter_rows(min_col=index[3] + 1, max_col=index[3] + 1) for person in i
               if person.value == distance and ws[person.row][index[4]].value == category]
 
+    # add points to participates in each category
+    add_points(people, sixth_race)
     return people
 
 
-people = get_file_arr()
-print(people)
+def get_complete_file_arr(excel_file: str = 'kuneticka_devitka.xlsx') -> list:
+    """
+    :param excel_file: File with the race results
+    :return: All participants of that race
+    """
+    people = [cat_people for param in dist_and_cat for cat_people in get_file_arr(excel_file, param[0], param[1])]
+    return people
+
+
+def get_final_arr(excel_file: str = 'final_table.xlsx') -> list:
+    """
+    :param excel_file: Excel file with the complete results
+    :return: List of all elements in there
+    """
+    try:
+        wb = opx.load_workbook(excel_file)
+        ws = wb.active
+    except InvalidFileException:
+        raise_error(excel_file, problem_message[2])
+    except FileNotFoundError:
+        raise_error(excel_file, problem_message[0])
+    else:
+        if len(ws.column_dimensions) == 0:
+            people = []
+        else:
+            people = [[person.value for person in element] for element in ws]
+
+    return people
--- my_utils.py
+++ my_utils.py
@@ -4,6 +4,15 @@ problem_message = ['We could not find your file, because the file does not exist
                    'I do not understand your command', 'We could not open your file, because the file can\'t be '
                    'open as an excel sheet', 'System error']
 set_cols_num = ['name', 'team', 'time', 'distance', 'category']
+dist_and_cat = [['Dlouhý okruh - 9 km', 'Muži 18 - 29 let'], ['Dlouhý okruh - 9 km', 'Muži 30 - 39 let'],
+            ['Dlouhý okruh - 9 km', 'Muži 40 - 49 let'], ['Dlouhý okruh - 9 km', 'Muži 50 - 59 let'],
+            ['Dlouhý okruh - 9 km', 'Muži 60 - 69 let'], ['Dlouhý okruh - 9 km', 'Muži 70 +'],
+            ['Dlouhý okruh - 9 km', 'Ženy 18 - 29 let'], ['Dlouhý okruh - 9 km', 'Ženy B (30 - 39 let)'],
+            ['Dlouhý okruh - 9 km', 'Ženy 40 - 49 let'], ['Dlouhý okruh - 9 km', 'Ženy 50 +'],
+            ['Krátký okruh - 4,5 km', 'Žáci 6 - 10 let'], ['Krátký okruh - 4,5 km', 'Žáci 11 - 14 let'],
+            ['Krátký okruh - 4,5 km', 'Junioři 15 - 17 let'], ['Krátký okruh - 4,5 km', 'Muži 18 +'],
+            ['Krátký okruh - 4,5 km', 'Žákyně 6 - 10 let'], ['Krátký okruh - 4,5 km', 'Žákyně 11 - 14 let'],
+            ['Krátký okruh - 4,5 km', 'Juniorky 15 - 17 let'], ['Krátký okruh - 4,5 km', 'Ženy 18 +']]
 
 
 def raise_error(file: str = '?unknown file?', message: str = 'No description of the error') -> None:
